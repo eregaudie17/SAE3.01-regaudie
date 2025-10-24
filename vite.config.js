0354c5a038e118a8b7b885dfa5a0015b7833d9a6
Emmanuel Regaudie <emmanuelregaudie5@gmail.com>
2025-10-24 14:22:39 +0200

    Init 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/repo/",
+  base: "SAE3.01-regaudie",
   build: {
     rollupOptions: {
       input: {
